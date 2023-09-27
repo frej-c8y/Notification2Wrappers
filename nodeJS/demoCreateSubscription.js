b5136e033704344f97cd5151960a5b8f5a9d13ef
Frederic Joulin <frederic.joulin@softwareag.com>
2023-09-27 18:42:37 +0200

    Clean up code

--- nodeJS/demoCreateSubscription.js
+++ nodeJS/demoCreateSubscription.js
@@ -8,10 +8,6 @@ let msgCount = 0;
 
 let subscription = undefined;
 (async function () {
-  // // Creation of a Service User
-  // // let { name: serviceUsername, password: servicePassword } = await createServiceUser(providerUrl, user, password, "wmio-connector-app");
-  // let serviceUsername = user;
-  // let servicePassword = password;
   const credentials = `${user}:${password}`;
   const credentials_base64 = Buffer.from(credentials);
 
@@ -28,8 +24,6 @@ let subscription = undefined;
       // .type("ProgramStatusChanged");
       .initialize();
     
-    // console.log('Subscription: ', JSON.stringify(subscription.subscription, null, '  '));
-
     startProcessingMessages();
   } catch (err) {
     console.log("Error catched in caller:", err);
@@ -46,30 +40,21 @@ function startProcessingMessages() {
 }
 
 async function processMessageCallback(message) {
-  // For test purposes: we stop as soon as at least one message is received and
-  // recreate a subscription and a websocket immediately afterwards
-  //subscription.closeWebsocket(true);
+  // For test purposes: we stop as soon as at least one message is received
 
   let notificationEventType = message.notificationHeaders[0].split("/")[2];
  
-//   message = _.pick(message, [ "ackHeader", "notificationHeaders[0]", "message.creationTime", "message.type", "message.severity", ]);
   console.log(`Message ${msgCount}:${JSON.stringify(message, null, 4)}`);
 
   const dateDiffInSec = (new Date().getTime() - Date.parse(message.message.time)) / 1000;
   console.log(`Message is ${dateDiffInSec} sec old`);
   msgCount++;
 
-/*  if (dateDiffInSec < 90 &&
-    (notificationEventType != Wrappernotification2wrapperALARMS ||
-      (notificationEventType === Wrappernotification2wrapperALARMS && message.message.severity === Wrappernotification2wrapperJOR)) &&
-    msgCount > 2
-  ) {*/
-    // For test purposes: we stop as soon as one message / one alarm with severity MAJOR is received
-    subscription.unsubscribe();
-    // Allow 5s extra time before closing the websocket to consume and acknowledge further pending incoming messages
-    await sleep(5000);
-    subscription.closeWebsocket(false);
-//  }
+  // For test purposes: we stop as soon as one message / one alarm with severity MAJOR is received
+  subscription.unsubscribe();
+  // Allow 5s extra time before closing the websocket to consume and acknowledge pending incoming messages
+  await sleep(5000);
+  subscription.closeWebsocket(false);
 }
 
 async function afterCloseCallback() {
