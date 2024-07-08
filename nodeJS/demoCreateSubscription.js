f4eeeafa68a6281c2151965199a4de92adfa188d
Frederic Joulin <frederic.joulin@softwareag.com>
2024-07-08 14:25:00 +0200

    Javascript - Test send emails

--- nodeJS/demoCreateSubscription.js
+++ nodeJS/demoCreateSubscription.js
@@ -56,6 +56,7 @@ async function processMessageCallback(message) {
   let notificationEventType = message.notificationHeaders[0].split("/")[2];
  
   console.log(`Message ${msgCount}:${JSON.stringify(message, null, 4)}`);
+  await sendEmail(message);
 
   const dateDiffInSec = (new Date().getTime() - Date.parse(message.message.lastUpdated)) / 1000;
   console.log(`Message is ${dateDiffInSec} sec old`);
@@ -70,4 +71,56 @@ async function processMessageCallback(message) {
 
 function sleep(milliseconds) {
   return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+function sendEmail(message) {
+//  emailProviderUrl = providerUrl;
+//  emailUser = user;
+//  emailPassword = password;
+  emailProviderUrl = 'https://oee-dev.eu-latest.cumulocity.com';
+  emailUser = 'frederic.joulin@softwareag.com';
+  emailPassword = 'Jsx:YsDsscf3Zwn';
+
+  let payload = {
+    "to": [
+      "frederic.joulin@softwareag.com",
+      "frederic.joulin@cumulocity.com"
+    ],
+    "cc": [],
+    "replyTo": "frej@softwareag.com",
+    "subject": "Message " + message.ackHeader + " received",
+    "text": "<pre>" + JSON.stringify(message.message, null, 4) + "</pre>"
+  };
+
+  return new Promise(function (resolve, reject) {
+    request(
+      {
+        method: "POST",
+        url: emailProviderUrl + '/email/emails',
+        auth: {
+          user: emailUser,
+          pass: emailPassword,
+          sendImmediately: true
+        },
+        json: true,
+        body: payload
+      },
+      function (err, res, body) {
+        if (err) {
+          reject(err);
+        }
+
+        const statusCode = _.get(res, "statusCode", -1);
+        switch (statusCode) {
+          case 200:
+            // console.log("response body: " + JSON.stringify(body));
+            console.log("Message successfully sent.");
+            resolve(body);
+            break;
+          default:
+            reject(`Email sending failed with status ${statusCode}`);
+        }
+      }
+    );
+  });
 }
